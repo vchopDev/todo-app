b4493802c5d3ebaebbf74a22d839e0ec7b5c4623
vchopDev <vchopao@gmail.com>
2022-06-05 17:17:25 +0200

    add docker

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { config } from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfig } from './databse.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
+      cache: true,
+      isGlobal: true,
+      load: [config]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseConfig
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ api/src/config.ts
@@ -0,0 +1,14 @@
+export const config = () => ({
+    database: {
+        type: 'mysql',
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        entities: [
+            __dirname + '/../**/*.entity{.ts,.js}',
+        ],
+        synchronize: true,
+    }
+});
